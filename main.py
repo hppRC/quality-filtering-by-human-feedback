80b6cab9cc628078529d716372350baa60eb46fa
hppRC <hpp.ricecake@gmail.com>
2023-07-12 15:54:40 +0900

    remove unsed dependencies

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
-import json
-import re
+
 import streamlit as st
 from datasets import load_dataset
 
 import utils
 
 MAX_EXAMPLES = 10000
-os.makedirs('annotations', exist_ok=True)
+os.makedirs("annotations", exist_ok=True)
 
 if "examples" not in st.session_state:
     mc4 = load_dataset("mc4", "ja", split="train", streaming=True)
@@ -20,35 +19,25 @@ if "examples" not in st.session_state:
     st.session_state["examples"] = examples
 
 # Check if index.txt exists and load it
-if os.path.exists('index.txt'):
-    with open('index.txt', 'r') as f:
+if os.path.exists("index.txt"):
+    with open("index.txt", "r") as f:
         index = int(f.read())
 else:
     index = 0
 
 # Create annotations directory if it does not exist
-if not os.path.exists('annotations'):
-    os.makedirs('annotations')
+if not os.path.exists("annotations"):
+    os.makedirs("annotations")
 
 # Determine current good and bad file indices
 good_file_index = index // MAX_EXAMPLES
 bad_file_index = index // MAX_EXAMPLES
 
-# Check if good file exists and load it
-good_file = f'annotations/good_{good_file_index}.jsonl'
-if os.path.exists(good_file):
-    with open(good_file, 'r') as f:
-        good = [json.loads(line) for line in f]
-else:
-    good = []
+good_file = f"annotations/good_{good_file_index}.jsonl"
+bad_file = f"annotations/bad_{bad_file_index}.jsonl"
 
-# Check if bad file exists and load it
-bad_file = f'annotations/bad_{bad_file_index}.jsonl'
-if os.path.exists(bad_file):
-    with open(bad_file, 'r') as f:
-        bad = [json.loads(line) for line in f]
-else:
-    bad = []
+good = utils.load_jsonl(good_file)
+bad = utils.load_jsonl(bad_file)
 
 st.session_state["index"] = index
 st.session_state["good"] = good
@@ -60,9 +49,9 @@ if st.button("許せる"):
     if len(st.session_state["good"]) >= MAX_EXAMPLES:
         st.session_state["good"] = []
         good_file_index += 1
-        good_file = f'annotations/good_{good_file_index}.jsonl'
+        good_file = f"annotations/good_{good_file_index}.jsonl"
     st.session_state["index"] += 1
-    with open('index.txt', 'w') as f:
+    with open("index.txt", "w") as f:
         f.write(str(st.session_state["index"]))
 
 if st.button("低クオリティ"):
@@ -71,15 +60,15 @@ if st.button("低クオリティ"):
     if len(st.session_state["bad"]) >= MAX_EXAMPLES:
         st.session_state["bad"] = []
         bad_file_index += 1
-        bad_file = f'annotations/bad_{bad_file_index}.jsonl'
+        bad_file = f"annotations/bad_{bad_file_index}.jsonl"
     st.session_state["index"] += 1
-    with open('index.txt', 'w') as f:
+    with open("index.txt", "w") as f:
         f.write(str(st.session_state["index"]))
 
 x = st.session_state["examples"][st.session_state["index"]]
-x0 = x['text'].replace('\n', '\n\n')
-x1 = x['timestamp'].replace('\n', '\n\n')
-x2 = x['url'].replace('\n', '\n\n')
+x0 = x["text"].replace("\n", "\n\n")
+x1 = x["timestamp"].replace("\n", "\n\n")
+x2 = x["url"].replace("\n", "\n\n")
 st.markdown(f"<div style='max-width: 800px;'>{x0}</div>", unsafe_allow_html=True)
 st.markdown(f"<div style='max-width: 800px;'>{x1}</div>", unsafe_allow_html=True)
 st.markdown(f"<div style='max-width: 800px;'>{x2}</div>", unsafe_allow_html=True)
--- utils.py
+++ utils.py
@@ -1,13 +1,8 @@
-import json
-import random
 from collections.abc import Iterable
-from inspect import ismethod
 from pathlib import Path
 from typing import Any
 
-import numpy as np
 import pandas as pd
-import torch
 
 
 def save_jsonl(data: pd.DataFrame | Iterable[dict] | dict[Any, Iterable], path: Path | str) -> None:
@@ -24,92 +19,8 @@ def save_jsonl(data: pd.DataFrame | Iterable[dict] | dict[Any, Iterable], path: 
     )
 
 
-def save_json(data: dict[Any, Any], path: Path | str) -> None:
+def load_jsonl(path: Path | str) -> list[dict]:
     path = Path(path)
-    path.parent.mkdir(parents=True, exist_ok=True)
-
-    with path.open("w") as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
-
-
-def load_jsonl(path: Path | str) -> pd.DataFrame:
-    path = Path(path)
-    return pd.read_json(path, lines=True)
-
-
-def load_json(path: Path | str) -> dict:
-    path = Path(path)
-    with path.open() as f:
-        data = json.load(f)
-    return data
-
-
-def log(data: dict, path: Path | str) -> None:
-    path = Path(path)
-    path.parent.mkdir(parents=True, exist_ok=True)
-
-    if path.exists():
-        df: pd.DataFrame = pd.read_csv(path)
-        df = pd.DataFrame(df.to_dict("records") + [data])
-        df.to_csv(path, index=False)
-    else:
-        pd.DataFrame([data]).to_csv(path, index=False)
-
-
-def save_config(data, path: Path | str) -> None:
-    path = Path(path)
-    path.parent.mkdir(parents=True, exist_ok=True)
-
-    try:
-        data: dict = data.as_dict()
-    except:
-        try:
-            data: dict = data.to_dict()
-        except:
-            pass
-
-    if type(data) != dict:
-        data: dict = vars(data)
-
-    data = {k: v for k, v in data.items() if not ismethod(v)}
-    data = {k: v if type(v) in [int, float, bool, None] else str(v) for k, v in data.items()}
-
-    save_json(data, path)
-
-
-def set_seed(seed: int = None) -> None:
-    if seed is None:
-        return
-
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
-
-def dict_average(dicts: Iterable[dict]) -> dict:
-    dicts: list[dict] = list(dicts)
-    averaged = {}
-
-    for k, v in dicts[0].items():
-        try:
-            v = v.item()
-        except:
-            pass
-        if type(v) in [int, float]:
-            averaged[k] = v / len(dicts)
-        else:
-            averaged[k] = [v]
-
-    for d in dicts[1:]:
-        for k, v in d.items():
-            try:
-                v = v.item()
-            except:
-                pass
-            if type(v) in [int, float]:
-                averaged[k] += v / len(dicts)
-            else:
-                averaged[k].append(v)
-
-    return averaged
+    if not path.exists():
+        return []
+    return pd.read_json(path, lines=True).to_dict(orient="records")
